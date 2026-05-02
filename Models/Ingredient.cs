73164f81b100fbf7958982a32660d97c6e915e77
hermes curo <hermescurov5@gmail.com>
2026-05-01 20:53:16 -0500

    Initial commit

---
+++ Models/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace break_back.Models;
+
+public partial class Ingredient
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public bool? IsAllergen { get; set; }
+
+    public virtual ICollection<Meal> Meals { get; set; } = new List<Meal>();
+}
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace break_back.Models;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual HealthProfile? HealthProfile { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<MedicalCondition> Conditions { get; set; } = new List<MedicalCondition>();
+}
