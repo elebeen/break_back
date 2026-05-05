32e052e097d8d643b0081b197f98b3d96188cd3f
hermes curo <hermescurov5@gmail.com>
2026-05-05 16:12:53 -0500

    Añadiendo un repositorio generico y unit of work

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace break_back.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    IEnumerable<TEntity> FindAll();
+    TEntity FindById(int id);
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+}
