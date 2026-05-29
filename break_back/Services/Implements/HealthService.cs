a882f7d14be2d1b57fb7d1df9fc1e1ec661fe32e
hermes curo <hermescurov5@gmail.com>
2026-05-28 21:31:51 -0500

    Refactorizando el proyecto para usar arquitectura hexagonal

--- break_back/Services/Implements/HealthService.cs
+++ break_back/Services/Implements/HealthService.cs
@@ -2,16 +2,19 @@ using break_back.Models;
 using break_back.Models.Dtos.HealthProfile;
 using break_back.Models.Dtos.User;
 using break_back.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace break_back.Services.Implements;
 
 public class HealthService : IHealthService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly Context _context;
 
-    public HealthService(IUnitOfWork unitOfWork)
+    public HealthService(IUnitOfWork unitOfWork,  Context context)
     {
         _unitOfWork = unitOfWork;
+        _context = context;
     }
 
     public async Task<HealthProfile> UpdateProfile(Guid userId, HealthProfileCreateDto profileData)
@@ -46,14 +49,27 @@ public class HealthService : IHealthService
 
     public async Task RemoveConditionFromUser(Guid userId, int conditionId)
     {
-        var user = _unitOfWork.Repository<User>().FindbyGuid(userId);
+       // var user = _unitOfWork.Repository<User>().FindbyGuid(userId);
 
         // obtener conditions de un usuario
-        var condition = await _unitOfWork.HealthRepository.GetConditionByUserId(userId, conditionId);
+        //var condition = await _unitOfWork.HealthRepository.GetConditionByUserId(userId, conditionId);
 
-        if (user != null && condition != null)
+        //if (user != null && condition != null)
+        //{
+        //    await _unitOfWork.HealthRepository.RemoveConditionFromUser(userId, conditionId);
+        //}
+        
+        var user = await _context.Users
+            .Include(u => u.Conditions
+                .Where(c => c.Id == conditionId))   // Solo cargamos la que queremos
+            .FirstOrDefaultAsync(u => u.Id == userId);
+
+        if (user?.Conditions.Any() == true)
         {
-            await _unitOfWork.HealthRepository.RemoveConditionFromUser(userId, conditionId);
+            var condition = user.Conditions.First();
+            user.Conditions.Remove(condition);
+        
+            await _unitOfWork.SaveChanges();
         }
     }
 
